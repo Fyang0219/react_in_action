67d130d679da9fb6b9efaeddf13cf9f66c231a5f
Fei Yang <feiiyang0219@gmail.com>
2017-10-03 11:06:09 -0400

    added 3.3 exercise

---
+++ 3.3.js
@@ -0,0 +1,43 @@
+const container = document.getElementById('ourFirstApp');
+
+class ShallowMerge extends React.Component {
+	constructor(props) {
+		super(props);
+	
+		this.state = {
+			user: {
+				name: "Mark",
+				colors: {
+					favorite: '',
+				}
+			}
+		};
+
+		this.onButtonClick = this.onButtonClick.bind(this);
+	}
+
+	onButtonClick() {
+		this.setState({
+			user: {
+				colors: {
+					favorite: 'blue'
+				}
+			}
+		});
+	}
+
+	render() {
+		return(
+			<div> 
+				<h1> My favorite color is {this.state.user.colors.favorite} and my name is {this.state.user.name}</h1>
+				<button onClick={this.onButtonClick}>show the color!</button>
+			</div>
+		)
+	}
+
+}
+
+ReactDOM.render(
+	<ShallowMerge incrementBy={1}/>,
+	container 
+);
