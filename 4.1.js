5f40ec38f4cff3038fe1716448fca97dc0073986
Fei Yang <feiiyang0219@gmail.com>
2017-10-05 14:31:23 -0400

    added 4.1 exercise

---
+++ 4.1.js
@@ -0,0 +1,50 @@
+const container = document.getElementById("ourFirstApp");
+
+class ChildComponent extends React.Component {
+
+	render() {
+		console.log('Child: render')
+		return (
+			<div>Props: {this.props.name}</div>
+		);
+	}
+}
+
+
+class ParentComponent extends React.Component {
+
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: 'give input'
+		};
+		this.onInputChange = this.onInputChange.bind(this); 
+	}
+
+
+	onInputChange(e) {
+		this.setState({ text: e.target.value });
+	}
+
+	render() {
+		console.log('Parent: render');
+
+		return (
+			<div className="container">
+				<h2> Learn about rendering and lifecycle methods!</h2>
+				<input 
+					value={this.state.text}
+					onChange={this.onInputChange}
+				/>
+				<ChildComponent name={this.state.text} />
+			</div>
+		);
+	}
+}
+
+
+ReactDOM.render(
+	<ParentComponent />,
+	container
+)
