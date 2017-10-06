4464821ce140b6b0844888c700f676d452a0361c
Fei Yang <feiiyang0219@gmail.com>
2017-10-05 23:01:31 -0400

    added 4.2 exercise

---
+++ 4.2.js
@@ -0,0 +1,56 @@
+const ChildComponent = React.createClass({
+	propTypes: {
+		name: React.PropTypes.string,
+	},
+
+	getInitialState: function() {
+		console.log("ChildComponent: getInitState");
+		return {
+			name: "mark",
+		}
+	},
+
+	render: function() {
+		console.log("ChildComponent: render");
+		return (
+			<div> Props: {this.props.name} </div>
+		);
+	}
+});
+
+const ParentComponent = React.createClass({
+	getDefaultProps: function() {
+		console.log('ParentComponent: getDefaultProps');
+	},
+
+	getInitialState: function() {
+		console.log('ParentComponent: getInitState');
+		return { text: ''};
+	},
+
+	onInputChange: function(e) {
+		this.setState({text: e.target.value})
+	},
+
+	render: function() {
+		console.log('ParentComponent: render');
+		return(
+			<div className="container">
+				<h2>Learn about rendering and lifecycle methods!</h2>
+				<input 
+					value={this.state.text}
+					onChange={this.onInputChange}
+				/>
+
+			<ChildComponent name={this.state.text} />
+			</div>
+			
+
+		)
+	}
+});
+
+ReactDOM.render(
+	<ParentComponent />,
+	document.getElementById("ourFirstApp")
+)
